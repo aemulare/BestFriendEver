f3f4be727d0f53d2ea3fe4dfbb1d8e99871597a1
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-01 19:37:29 -0400

    Bug fix

--- form-handler.php
+++ form-handler.php
@@ -1,15 +1,7 @@
 <?php
 // SENDGRID ==============================================
 require 'vendor/autoload.php';
-//Set SendGrid Credentials
 
-
-$sendgrid = new SendGrid("SENDGRID_API_KEY");
-
-
-//Create a new SendGrid Email object
-
-$mail = new SendGrid\Email();
 // =======================================================
 
 
@@ -29,7 +21,7 @@ $myemail = "maryika@ymail.com";
 
 // Receive and sanitize input
 $name = mysqli_real_escape_string($conn, $_POST['name']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
+$contact_email = mysqli_real_escape_string($conn, $_POST['email']);
 $phone = mysqli_real_escape_string($conn, $_POST['phone']);
 $message = mysqli_real_escape_string($conn, $_POST['message']);
 
@@ -39,7 +31,7 @@ if (!mysqli_query($conn,$sql)) {
 //echo "1 record added";
 
 // write to db
-$sql = "INSERT INTO form_submissions (name,phone,email,message) VALUES ('$name', '$phone','$email', '$message')";
+$sql = "INSERT INTO form_submissions (name,phone,email,message) VALUES ('$name', '$phone','$contact_email', '$message')";
 $result = $conn->query($sql);
 
 // Always set content-type when sending HTML email
@@ -47,22 +39,28 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 $info = "Thank you for your form submission. This is the copy of your message.\n\n
-         Email:\t\t$email\nName:\t\t$name\nPhone:\t\t$phone\n\n\nMessage:\t\t$message";
+         Email:\t\t$contact_email\nName:\t\t$name\nPhone:\t\t$phone\n\n\nMessage:\t\t$message";
 
 
 //SENDGRID ======================================================
-$subject = "New contact form submission";
-$senderid = "<webmaster@bestfriendever.com>";
-$email1 = new SendGrid\Email();
-$email1
-    ->addTo($email)
-    ->addBcc($myemail)
-    ->setFrom($senderid)
-    ->setSubject($subject)
-    ->setText($info);
+//Set SendGrid Credentials
 
 
-$sendgrid->send($email1);
+
+// Initialize the SendGrid object with your SendGrid credentials
+
+$sendgrid = new SendGrid('SENDGRID_API_KEY');
+
+//Create a new SendGrid Email object
+$email = new SendGrid\Email();
+$email
+    ->addTo($myemail)
+    ->addTo($contact_email)
+    ->setFrom($myemail)
+    ->setSubject('New contact from submission')
+    ->setText($message);
+
+$sendgrid->send($email);
 //SENDGRID ============================================================================
 
 function msg()
