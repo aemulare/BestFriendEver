72be6be8f604a4e4f02362fcb467485e84a43f9e
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-14 22:41:39 -0400

    Changes db connection

--- common_functions.php
+++ common_functions.php
@@ -3,17 +3,17 @@
 // opens db connection
 function OpenDBconnection()
 {
-    $servername = "127.0.0.1:8889";
-    $username = "root";
-    $password = "root";
-    $database = "bestfriendever";
+//    $servername = "127.0.0.1:8889";
+//    $username = "root";
+//    $password = "root";
+//    $database = "bestfriendever";
 
-//    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-//    $servername = $url["host"];
-//    $username = $url["user"];
-//    $password = $url["pass"];
-//    $database = substr($url["path"], 1);
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $servername = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 
 // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
