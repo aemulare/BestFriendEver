586cebf25b5a7df243534e0a53f2954a9ae774a0
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-15 10:32:50 -0400

    Clean up. Mock up data for users is changed with hashed passwords.

--- gallery-handler.php
+++ gallery-handler.php
@@ -12,14 +12,13 @@ if ($result->num_rows > 0)
     // output data of each row
     while($row = $result->fetch_assoc())
     {
-        echo "<div class=\"col-lg-4 col-md-6 col-sm-6 text-center dog\">
-                    <img class=\"img-responsive \" src=\"" . imageSource($row["picture"]) . "\" alt=\"\">
+        echo "<div class='col-lg-4 col-md-6 col-sm-6 text-center dog'>
+                    <img class='img-responsive' src='" . imageSource($row["picture"]) . "' alt=''>
                     <h3>" . $row["breed"] . "</h3>
               </div>";
     }
 }
 else echo "0 results";
-
 
 CloseDBconnection($conn);
 ?>
