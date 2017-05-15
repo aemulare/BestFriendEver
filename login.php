a51d993d9e0b39c1b5953dd5dacaf4479c927629
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-15 15:00:42 -0400

    Minor bug fix.

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST) )
     $email = test_input($_POST['email']);
     $password = test_input($_POST['password']);
 
-    if(!(empty($email) || empty($password)))
+    if(!empty($email) && !empty($password))
     {
         $conn = OpenDBconnection();
         $sql = " SELECT id FROM users WHERE email = '$email' AND password = sha1($password) ";
